4cbb8bdee6ce60af851359b5889ab083cc422c84
Renaldy Brada <renaldybrada@gmail.com>
2019-12-18 19:42:00 +0700

    except error then recursive

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ from services.quotes import Quotes
 import random
 
 def randomTweet():
-    twitter = Twitter()
-    quotes = Quotes()
-    quoteTypes = ["dad-jokes", "advices", "famous-quote"]
-    randIndex = random.randrange(0, 2, 1)
-    
-    if (quoteTypes[randIndex] == "dad-jokes"):
-        message = quotes.randomDadJokes()
-    elif (quoteTypes[randIndex] == "advices"):
-        message = quotes.randomAdvices()
-    else:
-        message = quotes.randomFamousQuote()
-    
-    twitter.postTweet(message)
+    try:
+        twitter = Twitter()
+        quotes = Quotes()
+        quoteTypes = ["dad-jokes", "advices", "famous-quote"]
+        randIndex = random.randrange(0, 2, 1)
+        
+        if (quoteTypes[randIndex] == "dad-jokes"):
+            message = quotes.randomDadJokes()
+        elif (quoteTypes[randIndex] == "advices"):
+            message = quotes.randomAdvices()
+        else:
+            message = quotes.randomFamousQuote()
+        
+        twitter.postTweet(message)
+        print('tweeting : ' + message)
+    except:
+        randomTweet()
 
 
 def randomRetweet():
-    twitter = Twitter()
-    timeline = twitter.getHomeTimeline()
-    randIndex = random.randrange(0, 200, 1)
-    tweet_id = timeline[randIndex]['id_str']
-    twitter.retweet(tweet_id)
+    try:
+        twitter = Twitter()
+        timeline = twitter.getHomeTimeline()
+        randIndex = random.randrange(0, 200, 1)
+        tweet_id = timeline[randIndex]['id_str']
+        
+        twitter.retweet(tweet_id)
+        print("retweeting : " + tweet_id)
+    except:
+        randomRetweet()
 
 idx = random.randrange(0, 20, 1)
 if(idx % 2 == 0):
