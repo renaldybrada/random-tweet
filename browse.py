79228219170d3bcf8d72f3a1623a8571e890fe1d
unknown <renaldybrada@gmail.com>
2020-03-22 10:07:47 +0700

    timeline pagination

--- browse.py
+++ browse.py
@@ -8,8 +8,17 @@ class Browse:
         { 'id' : 1, 'title' : 'Retweet a tweet' },
         { 'id' : 2, 'title' : 'Like a tweet' },
         { 'id' : 3, 'title' : 'Stalk someone timeline' },
+        { 'id' : 4, 'title' : 'Tweet to the world' },
         { 'id' : 99, 'title' : 'Get out from here! my boss is watching' }
     ]
+
+    # Tweet Pagination
+    tweet_limit = 200
+    per_page = 5
+
+    def printTitle(self, title):
+        text = pyfiglet.figlet_format(title)
+        print(text)
 
     # Showing available menu
     def showMenu(self):
@@ -25,38 +34,65 @@ class Browse:
         chooseMenu = input('What do you want ? [0, 1, 2] : ')
         print("")
         
-        self.menuController(int(chooseMenu))
+        self.menuController(chooseMenu)
 
     # Controlling menu to function
     def menuController(self, idMenu):
-        if idMenu == 0 :
+        if idMenu == "0" :
             self.showHomeTimeline()
-        elif idMenu == 1 :
+        elif idMenu == "1" :
             print('retweet under construction')
-        elif idMenu == 2 :
+        elif idMenu == "2" :
             print('like under construction')
-        elif idMenu == 3 :
+        elif idMenu == "3" :
             print('stalk under construction')
-        elif idMenu == 99 :
+        elif idMenu == "99" :
             exit()
+        else:
+            print('whoops!')
+            print("")
+            self.showMenu()
+
         print("")
 
     # showing timeline
     def showHomeTimeline(self):
+        self.printTitle('timeline')
+    
         twitter = Twitter()
-        timeline = twitter.getHomeTimeline(5)
-        for post in timeline :
-            print('username : ' + post['user']['screen_name'])
-            print(post['text'])
+        timeline = twitter.getHomeTimeline(self.tweet_limit)
+        
+        # pagination
+        current_page = 0
+        last_page = self.tweet_limit / self.per_page
+        
+        while current_page != last_page :
+            firstIndex = current_page * self.per_page
+            lastIndex = firstIndex + self.per_page
+            print('timeline page ' + str(current_page + 1))
+            print('') 
+            showTimeline = timeline[firstIndex:lastIndex]
+            
+            for post in showTimeline :
+                print('id : ' + post['id_str'])
+                print('username : ' + post['user']['screen_name'])
+                print(post['text'])
+                print("")
+                print(str(post['retweet_count']) + ' retweet, ' + str(post['favorite_count']) + ' like')
+                print(post['created_at'])
+                print("====================================================================================================================")
+                print("")
+                
+            action = input('press any key to continue or "m" to back to menu.. ')
             print("")
-            print(str(post['retweet_count']) + ' retweet, ' + str(post['favorite_count']) + ' like')
-            print(post['created_at'])
-            print("====================================================================================================================")
-            print("")
+            if(action == "m"):
+                self.showMenu()
+                break
 
+            current_page += 1
 
 # show title
-twitter_title = pyfiglet.figlet_format("Twitter-CLI")
+twitter_title = pyfiglet.figlet_format("Twitter-CLI", font = "slant")
 print(twitter_title)
 
 browse = Browse()
